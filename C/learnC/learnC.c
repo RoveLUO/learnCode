1a459332eaa3e022651a2418a0181c7630b6f5ca
FuLUO <774746695@qq.com>
2022-08-17 17:46:54 +0800

    @edit test

--- C/learnC/learnC.c
+++ C/learnC/learnC.c
@@ -1,17 +1,19 @@
-//#include <stdio.h>
-//
-//main()
-//{
-//	int fahr, celsius;
-//	int lower, upper, step;
-//	lower = 0;
-//	upper = 300;
-//	step = 20;
-//	fahr = lower;
-//	while (fahr <= upper)
-//	{
-//		celsius = 5 * (fahr - 32) / 9;
-//		printf("%d\t%d\n", fahr, celsius);
-//		fahr += step;
-//	}
-//}
+#include <stdio.h>
+// test
+main()
+{
+	int fahr, celsius;
+	int lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+	fahr = lower;
+	while (fahr <= upper)
+	{
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr += step;
+	}
+}
+
+// test
