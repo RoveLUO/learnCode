5163a21e877a47ceee56d7e2c882374d87d09155
FuLUO <774746695@qq.com>
2022-08-18 10:55:44 +0800

    @add new exercise

--- C/learnC/Chapter 1 - A Tutorial Introduction/Exercise 1-3.c
+++ C/learnC/Chapter 1 - A Tutorial Introduction/Exercise 1-3.c
@@ -1,6 +1,9 @@
+///*
+//	Exercise 1-3. Modify the temperature conversion program to print a heading above the table. 
+//*/
 //#include <stdio.h>
 //
-//main()
+//main() 
 //{
 //	int fahr;
 //	float celsius;
@@ -9,6 +12,7 @@
 //	upper = 300;
 //	step = 20;
 //	fahr = lower;
+//	printf("Fahrenheit Temperatures -> Centigrade\n");
 //	while (fahr <= upper)
 //	{
 //		celsius = 5 * (fahr - 32) / 9;
---
+++ C/learnC/Chapter 1 - A Tutorial Introduction/Exercise 1-4.c
@@ -0,0 +1,21 @@
+///*
+//	Exercise 1-4. Write a program to print the corresponding Celsius to Fahrenheit table. 
+//*/
+//
+//#include <stdio.h>
+//main() 
+//{
+//	int lower, upper, step;
+//	float fahr, celsius;
+//
+//	lower = -20;
+//	upper = 45;
+//	step = 5;
+//	celsius = lower;
+//	while (celsius <= upper)
+//	{
+//		fahr = celsius * 9 / 5 + 32;
+//		printf("%3.0f\t%6.1f\n", celsius, fahr);
+//		celsius += step;
+//	}
+//}
